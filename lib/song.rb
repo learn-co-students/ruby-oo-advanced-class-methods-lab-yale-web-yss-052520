e65e0c001cb8c796fbcad8cf642e0250889b05e0
Joao Cardoso <joao.cardoso@yale.edu>
2020-05-27 17:30:33 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,62 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+    attr_accessor :name, :artist_name
+    @@all = []
+    
+    def self.create
+      song = Song.new()
+      song.save
+      song
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def save
+      self.class.all << self
+    end
 
-  def self.all
-    @@all
+    def self.new_by_name(name)
+      song = Song.new()
+      song.name = name
+      song
+    end
+
+    def self.create_by_name(name)
+      song = Song.new()
+      song.name = name
+      song.save
+      song
+    end
+
+    def self.find_by_name(name)
+      self.all.find { |song| song.name == name}
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find_by_name(name) == nil ? self.create_by_name(name) : self.find_by_name(name)
+    end
+
+    def self.alphabetical
+      @@all.sort_by{|song| song.name}
+    end
+
+    def self.new_from_filename(filename)
+      song = Song.new()
+      array = filename.split(" - ")
+      array[1].slice!('.mp3')
+      song.artist_name = array[0]
+      song.name = array[1]
+      song
+    end
+
+    def self.create_from_filename(filename)
+      song = self.new_from_filename(filename)
+      song.save
+      song
+    end
+
+    def self.destroy_all
+      @@all = []
+    end
   end
-
-  def save
-    self.class.all << self
-  end
-
-end
