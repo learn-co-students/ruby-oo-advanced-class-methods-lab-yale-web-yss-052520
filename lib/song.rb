fe9f02ac2b3a527c26327ddc02e6603f97855685
ZIming Mao <ziming.mao@yale.edu>
2020-05-27 13:54:13 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,68 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    # @@all << song
+    song
+    
+  end
+
+  def self.create_by_name(name)
+    song =  self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |element|
+      if element.name == name
+        return element
+      end
+      
+    end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    song ||= self.create_by_name(name)
+    song
+  end
+
+  def self.alphabetical
+    sorted = @@all.sort_by(&:name)
+    sorted.map do |element|
+      element.name
+    end
+    sorted
+  end
+
+  def self.new_from_filename(filename)
+    arr = filename.split(/ - |.mp3/)
+    song = Song.new
+    song.name = arr[1]
+    song.artist_name = arr[0]
+    song
+    
+  end
+
+  def self.create_from_filename(filname)
+    song = self.new_from_filename(filname)
+    song.save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
+
 end
